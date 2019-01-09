aec3d5abd12a87d8dcca2125180d2bb7526d9edb
BartekSmalec <smalecbartlomiej@gmail.com>
2019-01-09 15:28:58 +0200

    added history to GUI

--- src/GUI/TopView.java
+++ src/GUI/TopView.java
@@ -22,7 +22,7 @@ public class TopView extends BorderPane {
 
     public TopView() {
 
-        name = new Text("Oxygen Network");
+        name = new Text("Twoja Stara");
         name.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 25));
         name.setFill(Color.DARKOLIVEGREEN);
 
